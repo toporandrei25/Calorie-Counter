da83e8955042bd8bee20dc362573138fa1f1682b
Andrei <andreitopor.2002@yahoo.com>
2023-07-04 17:25:15 +0300

    Database finished read/write

--- app/src/main/java/com/example/test/ActivityRegister.java
+++ app/src/main/java/com/example/test/ActivityRegister.java
@@ -13,11 +13,18 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class ActivityRegister extends AppCompatActivity {
@@ -25,8 +32,12 @@ public class ActivityRegister extends AppCompatActivity {
     private FirebaseAuth mAuth;
     public TextInputEditText editTextEmail;
     public TextInputEditText editTextPassword;
+    public TextInputEditText editTextGreutate;
+    public TextInputEditText editTextInaltime;
     public Button buttonRegister;
     private ProgressBar progressBar;
+
+    FirebaseFirestore firestore;
 
     @Override
     public void onStart() {
@@ -47,18 +58,23 @@ public class ActivityRegister extends AppCompatActivity {
         buttonBack = (Button) findViewById(R.id.butbck);
         editTextEmail = (TextInputEditText )findViewById(R.id.textEmail);
        editTextPassword =(TextInputEditText ) findViewById(R.id.textPass);
+       editTextGreutate = (TextInputEditText) findViewById(R.id.textGreutate);
+       editTextInaltime = (TextInputEditText) findViewById(R.id.textInaltime);
         buttonRegister = findViewById(R.id.btnsend);
         mAuth = FirebaseAuth.getInstance();
         progressBar = findViewById(R.id.progressBar);
+        firestore = FirebaseFirestore.getInstance();
 
         buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String email, password;
+                String inaltime,greutate;
                 progressBar.setVisibility(view.VISIBLE);
                 email =String.valueOf(editTextEmail.getText());
                 password = String.valueOf(editTextPassword.getText());
-
+                greutate = String.valueOf(editTextGreutate.getText());
+                inaltime = String.valueOf(editTextInaltime.getText());
 
                 if(TextUtils.isEmpty(email) && TextUtils.isEmpty(password)){
                     Toast.makeText(ActivityRegister.this,"Enter Email & Password",Toast.LENGTH_SHORT).show();
@@ -84,6 +100,26 @@ public class ActivityRegister extends AppCompatActivity {
                                     // Sign in success, update UI with the signed-in user's information
                                     Toast.makeText(ActivityRegister.this, "Acount created.",
                                             Toast.LENGTH_SHORT).show();
+                                    Map<String,Object> user = new HashMap<>();
+                                    user.put("Email", email);
+                                    user.put("Password", password);
+                                    user.put("Greutate",greutate);
+                                    user.put("Inaltime",inaltime);
+
+                                    firestore.collection("Users")
+                                            .add(user)
+                                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                                @Override
+                                                public void onSuccess(DocumentReference documentReference) {
+                                                    Toast.makeText(getApplicationContext(),"Success",Toast.LENGTH_LONG).show();
+                                                }
+                                            })
+                                            .addOnFailureListener(new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+                                                    Toast.makeText(getApplicationContext(),"Failure",Toast.LENGTH_LONG).show();
+                                                }
+                                            });
 
                                 } else {
                                     // If sign in fails, display a message to the user.
@@ -91,6 +127,7 @@ public class ActivityRegister extends AppCompatActivity {
                                             Toast.LENGTH_SHORT).show();
 
                                 }
+
                             }
                         });
 
--- app/src/main/java/com/example/test/Conected.java
+++ app/src/main/java/com/example/test/Conected.java
@@ -1,39 +1,97 @@
 package com.example.test;
 
+import static android.content.ContentValues.TAG;
 import static com.google.firebase.FirebaseApp.getInstance;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Map;
 
 public class Conected extends AppCompatActivity {
 
      FirebaseAuth auth;
     Button button;
-    TextView textView;
+    TextView textViewEmail;
+    TextView textViewPassword;
+    TextView textViewGreutate;
+    TextView textViewInaltime;
     FirebaseUser user;
+    FirebaseFirestore firestore;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conected);
+         String email,password;
+         int inaltime,greutate ;
         auth = FirebaseAuth.getInstance();
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
         button = findViewById(R.id.logout);
-        textView = findViewById(R.id.user_details);
+        textViewEmail = findViewById(R.id.user_email);//TEXTVIEW ID
+        textViewPassword = findViewById(R.id.user_password);
+        textViewGreutate = findViewById(R.id.user_greutate);
+        textViewInaltime = findViewById(R.id.user_inaltime);
+
         user = auth.getCurrentUser();
         if(user == null){
             Intent intent = new Intent(getApplicationContext(),MainActivity.class);
             startActivity(intent);
             finish();
         } else {
-            textView.setText(user.getEmail());
+
+            textViewEmail.setText(user.getEmail());//SET EMAIL
+
         }
+        email = String.valueOf(textViewEmail.getText());
+
+        firestore.collection("Users")
+                .whereEqualTo("Email", email)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+
+                                 String email = document.getString("Email");
+                                 String password = document.getString("Password");
+                                 String inaltime = document.getString("Inaltime");
+                                 String greutate = document.getString("Greutate");
+                                 textViewPassword.setText(password);
+                                 textViewInaltime.setText(inaltime);
+                                 textViewGreutate.setText(greutate);
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                           Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
+
+
+        //BUTTON LOG OOUT
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
